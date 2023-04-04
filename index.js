a91a0ba8184d52a9d46be14a725e5333cf46a4ef
おららん <81461409+orarange@users.noreply.github.com>
2023-04-04 16:36:28 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res){
+    res.render('index')
+})
+
+app.listen(8080, function () {});
