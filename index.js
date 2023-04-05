82a7074089e13250e6d5a9a8a2650f762ac06109
おららん <81461409+orarange@users.noreply.github.com>
2023-04-05 18:19:54 +0000

    一応完成

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/',function(req,res){
     res.render('index')
 })
